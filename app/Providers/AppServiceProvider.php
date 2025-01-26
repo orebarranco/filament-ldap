ec26ce00ed7d8c7be0ba0c10bc99ee3586c087c7
Oreste Barranco Hernández <orebarranco90@gmail.com>
2025-01-25 21:57:53 -0500

    Configurar seguridad y manejo de fechas en AppServiceProvider
    - Se prohíben los comandos destructivos en producción para evitar
    riesgos en la base de datos.
    - Se habilita el modo estricto en los modelos de Eloquent en entornos de
     desarrollo para mejorar la detección de errores.
    - Se actualizan las reglas de validación de contraseñas en producción,
    requiriendo contraseñas de al menos 8 caracteres y no comprometidas.
    - Se configura el uso de CarbonImmutable para el manejo de fechas,
    evitando modificaciones accidentales de objetos de fecha.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,43 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configurePasswordValidation();
+        $this->configureDates();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+    }
+
+    /**
+     * Configure the dates.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Configure the models.
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! $this->app->isProduction());
+    }
+
+    /**
+     * Configure the password validation rules.
+     */
+    private function configurePasswordValidation(): void
+    {
+        Password::defaults(fn () => $this->app->isProduction() ? Password::min(8)->uncompromised() : null);
     }
 }
